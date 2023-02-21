0ee26b5f6699f5851ed45bb12b52d602b4579694
Justin Chiu <chiu.justin.t@gmail.com>
2023-02-21 18:17:20 -0500

    .

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+import numpy as np
 import streamlit as st
 
 with Path("doc2dial_dial_train.json").open() as f:
@@ -24,4 +25,28 @@ for turn in turns:
     st.write(utterance)
     for reference in references:
         st.write(reference["sp_id"])
-import pdb; pdb.set_trace()
+
+def doc_dial_lengths(dials, docs):
+    dial_lens = []
+    for domain, domdials in dials.items():
+        domdocs = docs[domain]
+        for docname, dials in domdials.items():
+            for dial in dials:
+                dialtext = " ".join([
+                    f'{turn["role"]}: {turn["utterance"]}'
+                    for turn in dial["turns"]
+                ])
+                dial_lens.append(len(dialtext.strip().split()))
+
+    doc_lens = []
+    for domain, domdocs in docs.items():
+        for doc in domdocs.values():
+            title = doc["title"]
+            text = doc["doc_text"]
+            doc_lens.append(len(title.split()) + len(text.split()))
+
+    print(np.min(dial_lens), np.mean(dial_lens), np.max(dial_lens))
+    print(np.min(doc_lens), np.mean(doc_lens), np.max(doc_lens))
+
+
+doc_dial_lengths(dials["dial_data"], docs["doc_data"])
